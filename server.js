015b3a82e4c5f063b7b9ff686821794be2d0aa7e
Raccoonyz <37939506+Raccoonyz@users.noreply.github.com>
2021-01-11 16:51:35 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
-  
 const yourUserId = ""; // put your user id here
 const robloSecurity = ""; // put roblosecurity token here
 
+const whitelist = ""; // whitelist of user ids separated by a comma; none of the users in the list will be unfriended. example is 0000,0000,0000
+
 const noblox = require("noblox.js");
+
+let whitelistArray = whitelist.split(",");
+
 function loop(f, number) {
   setTimeout(function() {
     console.log(number);
-    noblox.removeFriend(f[number].id);
+    if (whitelistArray[0] == "") {
+      noblox.removeFriend(f[number].id);
+    } else if (whitelistArray.indexOf(f[number].id) == -1) {
+      noblox.removeFriend(f[number].id);
+    }
     loop(f, number + 1);
   }, 500);
 }
@@ -16,7 +24,7 @@ async function run() {
   console.log("Bot is up");
   var friends = noblox.getFriends(yourUserId).then(friends => {
     loop(friends.data, 0);
-  })
+  });
 }
 
 run();
